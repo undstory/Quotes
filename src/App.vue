d066a774573d0556252010a9a0f269b14c57499c
undstory <undrul.ozog@gmail.com>
2020-11-16 21:28:34 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/Random">Random</router-link>
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
@@ -9,24 +10,8 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
+@import "./styles/variables.scss";
+@import "./styles/mixins.scss";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/views/Random.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Random quotes will be soon right here!
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: "Random"
+})
+</script>
