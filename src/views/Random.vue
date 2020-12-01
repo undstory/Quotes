e90dd5b9505562808aa4b97c93f00405e2ac7fa8
undstory <undrul.ozog@gmail.com>
2020-12-01 12:49:52 +0100

    small cosmetics

--- src/views/Random.vue
+++ src/views/Random.vue
@@ -2,8 +2,8 @@
     <div class="w-full h-screen bg-gray-600 text-3xl text-white-100 text-center">
       
         <!-- <div v-for="quote in quotes" v-bind:key="quote.id">{{ quote.price }}</div> -->
-        <div class="px-2 pt-10 pb-5 w-1/2 text-center m-auto">"{{ findRandom.text }}"</div>
-        <div class="px-2 py-3 text-2xl">{{ findRandom.author }}</div>
+        <div class="px-2 pt-10 pb-5 w-3/4 text-center m-auto">"{{ findRandom.text }}"</div>
+        <div class="px-2 py-3 text-2xl"><span class="author">{{ findRandom.author }}</span></div>
         <button type="button" @click="ile" class="border-solid border-4 border-pink-400 py-2 mx-1 my-8 px-4 md:w-1/5 w-1/5 text-xl text-center hover:bg-pink-400 hover:font-semibold">Give Me Another One</button>
     </div>
 </template>
@@ -44,3 +44,21 @@ export default ({
     }
 });
 </script>
+
+<style scoped>
+
+.author {
+    position: relative;
+   z-index: 555;
+}
+
+.author::after {
+    content: " ";
+    border-bottom: 10px solid rgb(96, 165, 250);
+    position: absolute;
+    width: 100%;
+    bottom: 3px;
+    left: 0;
+    z-index: -1;
+}
+</style>
