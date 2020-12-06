e7e5b20ef435359c294868e13c39249c1def887c
undstory <undrul.ozog@gmail.com>
2020-12-06 13:14:42 +0100

    filter option in search

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,13 +1,5 @@
 export default {
 
-    // findRandom: state => {
-    //     const random = Math.floor(Math.random()*state.quotes.length+1);
-    //     // const currentQuote = state.quotes[random];
-    //     // console.log(currentQuote);
-    //     // return currentQuote;
-    //     return random;
-    // },
-    
     random(state) {
         return state.random;
     },
@@ -17,7 +9,19 @@ export default {
 
     computeId(state) {
         return state.quotes.length;
-    }
+    },
+
+    filteredQuotesAuthor(state) {
+        return state.quotes.map(quote => quote.author).sort();
+    },
+
+    filteredQuotesCategory(state) {
+        return state.quotes.map(quote => quote.category).sort();
+    },
+
+    // filteredByAuthor(state, getters) {
+    //     return this.filteredQuotesAuthor.value.map()
+    // } 
 
     
 }
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,19 +4,41 @@
         <div class="container flex mx-auto flex-col sm:flex-row py-4 w-1/2 justify-between items-center">
             
             <select class="bg-transparent border-solid border-4 border-pink-400 py-2 mx-1 my-8 px-4 md:w-2/5 w-3/5 text-xl text-center hover:bg-pink-400 hover:font-semibold" name="Author" id="Author">
-                <option>Author</option>
+                <option class="bg-gray-600 text-white-100">Author:</option>
+                <option class="bg-gray-600 text-white-100" v-for="author in filteredQuotesAuthor" v-bind:key="author"  :value="author">{{ author }}</option>
             </select>
 
+
             <select class="bg-transparent border-solid border-4 border-pink-400 py-2 mx-1 my-8 px-4 md:w-2/5 w-3/5 text-xl text-center hover:bg-pink-400 hover:font-semibold" name="" id="">
-                <option>Category</option>
+                <option class="bg-gray-600 text-white-100">Category:</option>
+                <option class="bg-gray-600 text-white-100" v-for="category in filteredQuotesCategory" v-bind:key="category">{{ category }}</option>
         </select>
         </div>
+
+    <div>
+        <ul>Tu wyświetl:
+            <li></li>
+
+        </ul>
+    </div>
         
     </div>
 </template>
 
 <script>
 export default {
-    name: 'search'
+    name: 'search',
+    computed: {
+    
+        filteredQuotesAuthor() {
+            return [... new Set(this.$store.getters.filteredQuotesAuthor)];
+        },
+
+         filteredQuotesCategory() {
+            return [...new Set(this.$store.getters.filteredQuotesCategory)];
+        },
+
+    
+    }
 }
 </script>
